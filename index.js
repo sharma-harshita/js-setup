12b712d225773e125756dd009d39a39a52fe567b
Harshita Sharma <sharmaharshita711@gmail.com>
2024-02-08 23:46:08 +0530

    changes

---
+++ index.js
@@ -0,0 +1,35 @@
+// console.log("here inside JS file");
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+
+
+// // console.log(b);
+// let c = 20;
+// let b;
+// b = 20;
+// b = "HEllo"
+
+// console.log(b);
+
+// // console.log(d);
+// const d = "hey";
+// console.log(d);
+
+
+
+var a = 10;
+
+function add (){
+    let b = 20;
+    console.log("here",b, a, c);
+    
+    if(2<4){
+        const c = 30;
+        console.log("here c", c, b);
+    }
+}
+add()
+console.log(a);
+console.log(b);
